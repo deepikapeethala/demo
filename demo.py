7579c0c9dc28d570362b234cd388b2cb557cbd20
deepikapeethala <124231113+deepikapeethala@users.noreply.github.com>
2023-12-29 14:48:16 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,15 +1,16 @@
 import streamlit as st
-st.set_page_config(page_title='cats')
-st.markdown("## Types of Cats")
+st.set_page_config(page_title = 'cats')
+st.markdown("Types of Cats")
+
 col1,col2=st.columns(2)
 with col1:
-  st.subheader("Persian cat")
-  st.image("/054d7111.jpg",caption="Persian cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.write("Persian Cat")
+  st.subheader("/054d7111.jpg",caption="Persian Cat",width=300,use_column_width=True)
+  st.write("Persian cat are cute")
 with col2:
-  st.subheader("White cat")
-  st.image("./1013332-white-cats-wallpaper-1920x1200-for-meizu.jpg,caption="White cat",width=300,use_column_width=True)
-  st.write("White cats are cute")
+  st.write("white Cat")
+  st.subheader("./1013332-white-cats-wallpaper-1920x1200-for-meizu.jpg",caption="White Cat",width=300,use_column_width=True)
+  st.write("white cat are cute")
 
 
 
