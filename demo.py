f368302f2aff0565e73282f9b6ad03e89fc5594a
deepikapeethala <124231113+deepikapeethala@users.noreply.github.com>
2023-12-29 12:55:38 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,11 +5,11 @@ st.header("Types of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.write("Persian Cat")
-  st.subheader("/054d7111.jpg",caption="Persian Cat",width=300,use_columns_width=True)
+  st.subheader("/054d7111.jpg",caption="Persian Cat",width=300,use_column_width=True)
   st.write("Persian cat are cute")
 with col2:
   st.write("white Cat")
-  st.subheader("./1013332-white-cats-wallpaper-1920x1200-for-meizu.jpg",caption="white Cat",width=300,use_columns_width=True)
+  st.subheader("./1013332-white-cats-wallpaper-1920x1200-for-meizu.jpg",caption="White Cat",width=300,use_column_width=True)
   st.write("white cat are cute")
 
 
