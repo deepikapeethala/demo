58ccec59609966fa4ca1e334bf52fc892dc63920
deepikapeethala <124231113+deepikapeethala@users.noreply.github.com>
2023-12-29 12:03:17 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,6 +6,6 @@ st.image("./054d7111.jpg")
 st.write("white Cat")
 st.image("./1013332-white-cats-wallpaper-1920x1200-for-meizu.jpg")
 st.write("balck Cat")
-st.image("./https://img.theepochtimes.com/assets/uploads/2020/03/05/cat-033496.jpg")
+st.image("./https://wallpaperaccess.com/full/105675.jpg")
 
 
