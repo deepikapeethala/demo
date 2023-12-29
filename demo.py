8807cfd5ad96830428fdb5278eeca736aacbd345
deepikapeethala <124231113+deepikapeethala@users.noreply.github.com>
2023-12-29 12:05:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,6 @@ st.write("Persian Cat")
 st.image("./054d7111.jpg")
 st.write("white Cat")
 st.image("./1013332-white-cats-wallpaper-1920x1200-for-meizu.jpg")
-st.write("balck Cat")
-st.image("./https://wallpaperaccess.com/full/105675.jpg")
+
 
 
